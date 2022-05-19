317f13b65fefd46d38b52a38b82df99954384cdc
ml910 <m.lopatka123@gmail.com>
2022-05-19 23:18:40 +0200

    L3E1 - Create TeamMemberDTO inside team lib - note on VS Code

--- projects/team/src/lib/adapters/primary/components/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/components/our-team.component.ts
@@ -1,3 +1,4 @@
+import { TeamMemberDTO } from './../../../application/ports/secondary/dto/team-member.dto';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -5,13 +6,6 @@ import {
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
-
-interface TeamMember {
-  name: string;
-  surname: string;
-  imageUrl: string;
-  description: string;
-}
 
 // Here
 // TODO: This is not a lib, it's a component inside a lib
@@ -24,17 +18,17 @@ interface TeamMember {
 export class OurTeamComponent {
   constructor(private _route: ActivatedRoute, private _router: Router) {}
 
-  teamMembers$: Observable<TeamMember[]> = of([
+  teamMembers$: Observable<TeamMemberDTO[]> = of([
     {
+      id: '1',
       name: 'John',
-      surname: 'McJohn',
       imageUrl:
         'https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934',
       description: 'A happy guy',
     },
     {
+      id: '2',
       name: 'Jane',
-      surname: "O'Jane",
       imageUrl:
         'https://images.unsplash.com/photo-1607990283143-e81e7a2c9349?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1141',
       description: 'A smiley woman',
