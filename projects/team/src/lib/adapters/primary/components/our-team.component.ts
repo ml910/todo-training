3f229ab5075490b70c10759e2f8119836cab7a10
ml910 <m.lopatka123@gmail.com>
2022-05-20 15:14:00 +0200

    Reverting changes for DTO that should not be there - notes left about instruction manual/video

--- projects/team/src/lib/adapters/primary/components/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/components/our-team.component.ts
@@ -6,7 +6,6 @@ import {
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { COMPANY_DEPARTMENTS } from '../../../application/ports/secondary/dto/department.dto';
 
 // Here
 // TODO: This is not a lib, it's a component inside a lib
@@ -26,9 +25,6 @@ export class OurTeamComponent {
       imageUrl:
         'https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934',
       description: 'A happy guy',
-      department: {
-        name: COMPANY_DEPARTMENTS.IT,
-      },
     },
     {
       id: '2',
@@ -36,9 +32,6 @@ export class OurTeamComponent {
       imageUrl:
         'https://images.unsplash.com/photo-1607990283143-e81e7a2c9349?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1141',
       description: 'A smiley woman',
-      department: {
-        name: COMPANY_DEPARTMENTS.ACCOUNTING,
-      },
     },
   ]);
 
--- projects/team/src/lib/application/ports/secondary/dto/team-member.dto.ts
+++ projects/team/src/lib/application/ports/secondary/dto/team-member.dto.ts
@@ -1,9 +1,6 @@
-import { DepartmentDTO } from './department.dto';
-
 export interface TeamMemberDTO {
   readonly id: string;
   readonly name: string;
   readonly description: string;
   readonly imageUrl: string;
-  department: DepartmentDTO;
 }
