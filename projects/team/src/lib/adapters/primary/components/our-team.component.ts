101212818eeb339268f8e0978f80fd6a5e5840cd
ml910 <m.lopatka123@gmail.com>
2022-05-20 15:17:19 +0200

    description to bio

--- projects/team/src/lib/adapters/primary/components/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/components/our-team.component.ts
@@ -24,14 +24,14 @@ export class OurTeamComponent {
       name: 'John',
       imageUrl:
         'https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934',
-      description: 'A happy guy',
+      bio: 'A happy guy',
     },
     {
       id: '2',
       name: 'Jane',
       imageUrl:
         'https://images.unsplash.com/photo-1607990283143-e81e7a2c9349?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1141',
-      description: 'A smiley woman',
+      bio: 'A smiley woman',
     },
   ]);
 
--- projects/team/src/lib/application/ports/secondary/dto/team-member.dto.ts
+++ projects/team/src/lib/application/ports/secondary/dto/team-member.dto.ts
@@ -1,6 +1,6 @@
 export interface TeamMemberDTO {
   readonly id: string;
   readonly name: string;
-  readonly description: string;
+  readonly bio: string;
   readonly imageUrl: string;
 }
